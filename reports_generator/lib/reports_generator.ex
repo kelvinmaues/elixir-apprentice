cce11d8c672d70d52e17265ded93898e71ce2b56
Kelvin Maués <kgmdeveloper@gmail.com>
2022-09-19 16:01:01 -0300

    feat: switch case with pattern matching

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -1,12 +1,23 @@
-defmodule ReportsGenerator do alias ReportsGenerator.Parser
+# defmodule ReportsGenerator do alias ReportsGenerator.Parser
+#   def build(filename) do
+#     filename
+#     |> Parser.parse_file()
+#     |> Enum.reduce(%{}, fn line, acc_report -> sum_price_values(line, acc_report) end)
+#   end
+
+#   defp sum_price_values(line, acc_report) do
+#     [id, _food_name, price] = line
+#     Map.put(acc_report, id, (acc_report[id] || 0) + price)
+#   end
+# end
+
+defmodule ReportsGenerator do
   def build(filename) do
-    filename
-    |> Parser.parse_file()
-    |> Enum.reduce(%{}, fn line, acc_report -> sum_price_values(line, acc_report) end)
+    case File.read("reports/#{filename}") do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+      _ -> "Unknown error"
+    end
   end
 
-  defp sum_price_values(line, acc_report) do
-    [id, _food_name, price] = line
-    Map.put(acc_report, id, (acc_report[id] || 0) + price)
-  end
 end

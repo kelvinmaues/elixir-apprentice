26625c7732a459c8178a68869f99f00a0b1d3e78
Kelvin Maués <kgmdeveloper@gmail.com>
2022-09-19 11:49:01 -0300

    feat: new elixir repos

---
+++ reports_generator/lib/reports_generator.ex
@@ -0,0 +1,12 @@
+defmodule ReportsGenerator do alias ReportsGenerator.Parser
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(%{}, fn line, acc_report -> sum_price_values(line, acc_report) end)
+  end
+
+  defp sum_price_values(line, acc_report) do
+    [id, _food_name, price] = line
+    Map.put(acc_report, id, (acc_report[id] || 0) + price)
+  end
+end

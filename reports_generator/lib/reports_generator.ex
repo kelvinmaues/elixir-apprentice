34434fc7113574abeb7d5f298272745911df8d9c
Kelvin Maués <kgmdeveloper@gmail.com>
2022-09-19 16:12:54 -0300

    feat: applying pipe operator to handle file

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -13,11 +13,11 @@
 
 defmodule ReportsGenerator do
   def build(filename) do
-    case File.read("reports/#{filename}") do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-      _ -> "Unknown error"
-    end
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file()
   end
 
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error reading file!"
 end

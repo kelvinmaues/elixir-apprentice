ba2da41284bcdf9b8b337bdede25e20386097e7d
Kelvin Maués <kgmdeveloper@gmail.com>
2022-10-12 20:05:03 -0300

    feat(order): seed for order

--- exlivery/test/support/factory.ex
+++ exlivery/test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
 
   def user_factory do
     %User{
@@ -20,6 +20,24 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 2,
       unit_price: Decimal.new("45.50")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua das Grumixamas",
+      items: [
+        build(:item),
+        build(
+          :item,
+          title: "X-burguer",
+          category: :hamburguer,
+          quantity: 2,
+          unit_price: Decimal.new("15.90")
+        )
+      ],
+      total_price: Decimal.new("122.80"),
+      user_cpf: "123456789"
     }
   end
 end

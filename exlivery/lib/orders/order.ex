9e6644b384de389c26797a8ce1d486551389d42d
Kelvin Maués <kgmdeveloper@gmail.com>
2022-10-11 21:30:35 -0300

    feat(order): generate order by user and items

--- exlivery/lib/orders/order.ex
+++ exlivery/lib/orders/order.ex
@@ -1,18 +1,36 @@
 defmodule Exlivery.Orders.Order do
+  alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
   @keys [:user_cpf, :delivery_address, :items, :total_price]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build do
+  def build(%User{cpf: cpf, address: address}, [%Item{} | _items] = items) do
+    total_price = calculate_total_price(items)
+
     {
       :ok,
       %__MODULE__{
-        user_cpf: nil,
-        delivery_address: nil,
-        items: nil,
-        total_price: nil
+        user_cpf: cpf,
+        delivery_address: address,
+        items: items,
+        total_price: total_price
       }
     }
+  end
+
+  def build(_user, _items), do: {:error, "Invalid parameters"}
+
+  defp calculate_total_price(items) do
+    acc_value = Decimal.new("0.00")
+    Enum.reduce(items, acc_value, &sum_prices(&1, &2))
+  end
+
+  defp sum_prices(%Item{unit_price: price, quantity: quantity}, acc_value) do
+    price
+    |> Decimal.mult(quantity)
+    |> Decimal.add(acc_value)
   end
 end

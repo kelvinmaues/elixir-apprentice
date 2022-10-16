8447bf7239e0663b4dea4a325d17bd444fceba10
Kelvin Maués <kgmdeveloper@gmail.com>
2022-10-15 22:40:04 -0300

    feat(facade): create order by delegation

--- exlivery/lib/exlivery.ex
+++ exlivery/lib/exlivery.ex
@@ -1,11 +1,16 @@
 defmodule Exlivery do
+  alias Exlivery.Orders
   alias Exlivery.Users
+  alias Orders.Agent, as: OrderAgent
+  alias Orders.CreateOrUpdate, as: CreateOrUpdateOrder
   alias Users.Agent, as: UserAgent
-  alias Users.CreateOrUpdate
+  alias Users.CreateOrUpdate, as: CreateOrUpdateUser
 
   def start_agents do
     UserAgent.start_link()
+    OrderAgent.start_link()
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrder, as: :call
 end
